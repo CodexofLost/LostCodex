0b4b2de90a0c564e5782e9f2307ee0e2eedc46c4
BrainrotingCoding <thindistheking@gmail.com>
2025-06-28 19:46:58 +0530

    tile testing

--- app/src/main/java/com/save/me/tile/LaunchAppTileService.kt
+++ app/src/main/java/com/save/me/tile/LaunchAppTileService.kt
@@ -1,10 +1,17 @@
 package com.save.me.tile
 
 import android.content.Intent
+import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 import com.save.me.MainActivity
 
 class LaunchAppTileService : TileService() {
+    override fun onStartListening() {
+        super.onStartListening()
+        qsTile?.state = Tile.STATE_ACTIVE
+        qsTile?.updateTile()
+    }
+
     override fun onClick() {
         super.onClick()
         val intent = Intent(this, MainActivity::class.java).apply {
